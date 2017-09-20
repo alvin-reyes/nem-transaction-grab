a8d6b1063c978b62328e3af42b4b31fcaa18bc89
BramBear <alvin.reyes@botmill.io>
2017-09-20 00:08:39 -0400

    Modified with new features

--- src/main/java/io/nem/apps/tg/TransactionService.java
+++ src/main/java/io/nem/apps/tg/TransactionService.java
@@ -10,48 +10,22 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-import org.apache.commons.io.FileUtils;
-import org.nem.core.crypto.Hash;
-import org.nem.core.crypto.KeyPair;
-import org.nem.core.crypto.PrivateKey;
-import org.nem.core.crypto.PublicKey;
-import org.nem.core.messages.SecureMessage;
-import org.nem.core.model.Account;
-import org.nem.core.model.HashUtils;
-import org.nem.core.model.MultisigSignatureTransaction;
-import org.nem.core.model.TransferTransaction;
-import org.nem.core.model.ncc.NemAnnounceResult;
-import org.nem.core.model.ncc.TransactionMetaDataPair;
+import org.nem.core.model.mosaic.MosaicFeeInformation;
+import org.nem.core.model.mosaic.MosaicId;
+import org.nem.core.model.namespace.NamespaceId;
 import org.nem.core.model.primitive.Amount;
-import org.nem.core.node.NodeEndpoint;
-import org.nem.core.serialization.JsonSerializer;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.github.opendevl.JFlat;
-
-import io.nem.apps.api.TransactionApi;
-import io.nem.apps.builders.ConfigurationBuilder;
-import io.nem.apps.builders.MultisigSignatureTransactionBuilder;
-import io.nem.apps.builders.MultisigTransactionBuilder;
-import io.nem.apps.builders.TransferTransactionBuilder;
-import io.nem.apps.crypto.SecureMessageDecoder;
-import io.nem.apps.crypto.SecureMessageEncoder;
-import io.nem.apps.factories.AttachmentFactory;
+import io.nem.apps.model.NISQuery;
 import io.nem.apps.tg.utils.KeyConvertor;
 import io.nem.apps.util.Constants;
-import io.nem.apps.util.GzipUtils;
 import io.nem.apps.util.HexStringUtils;
-import io.nem.apps.util.JsonUtils;
 import io.nem.apps.util.NetworkUtils;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
@@ -63,12 +37,31 @@ import net.sf.json.JSONObject;
 		"http://botmill.io" })
 public class TransactionService {
 
-
+	private static Long totalValue = 0l;
+	private static Long totalIncomingValue = 0l;
+	private static Long totalOutgoingValue = 0l;
 	private static String host = "alice2.nem.ninja";
-
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/grab/total")
+	public ResponseEntity<String> grabTotal() {
+		return ResponseEntity.accepted().contentType(MediaType.APPLICATION_JSON).body(String.valueOf(totalValue));
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/grab/incoming/total")
+	public ResponseEntity<String> grabIncomingTotal() {
+		return ResponseEntity.accepted().contentType(MediaType.APPLICATION_JSON).body(String.valueOf(totalIncomingValue));
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/grab/outgoing/total")
+	public ResponseEntity<String> grabOutgoingTotal() {
+		return ResponseEntity.accepted().contentType(MediaType.APPLICATION_JSON).body(String.valueOf(totalOutgoingValue));
+	}
+	
 	@RequestMapping(method = RequestMethod.GET, path = "/address/{address}")
 	public ResponseEntity<String> grabLatest(@PathVariable String address) {
-
+		totalValue = 0l;
+		totalIncomingValue = 0l;
+		totalOutgoingValue = 0l;
 		net.sf.json.JSONObject json = net.sf.json.JSONObject.fromObject(
 				NetworkUtils.get("http://" + host + ":7890/account/transfers/all?address=" + address).getResponse());
 		net.sf.json.JSONArray jsonArrayMeta = json.getJSONArray("data");
@@ -83,9 +76,6 @@ public class TransactionService {
 		String jsonData = "";
 		while (itr.hasNext()) {
 			JSONObject element = (JSONObject) itr.next();
-
-			// lastHash =
-			// element.getJSONObject("meta").getJSONObject("hash").getString("data");
 			jsonData += parse(address, element.toString(), 0);
 		}
 		jsonData += recurse(address, lastHash, 0);
@@ -96,7 +86,9 @@ public class TransactionService {
 
 	@RequestMapping(method = RequestMethod.GET, path = "/incoming/address/{address}")
 	public ResponseEntity<String> grabIncomingOnly(@PathVariable String address) {
-
+		totalValue = 0l;
+		totalIncomingValue = 0l;
+		totalOutgoingValue = 0l;
 		net.sf.json.JSONObject json = net.sf.json.JSONObject.fromObject(
 				NetworkUtils.get("http://" + host + ":7890/account/transfers/all?address=" + address).getResponse());
 		net.sf.json.JSONArray jsonArrayMeta = json.getJSONArray("data");
@@ -111,9 +103,6 @@ public class TransactionService {
 		String jsonData = "";
 		while (itr.hasNext()) {
 			JSONObject element = (JSONObject) itr.next();
-
-			// lastHash =
-			// element.getJSONObject("meta").getJSONObject("hash").getString("data");
 			jsonData += parse(address, element.toString(), 1);
 		}
 
@@ -125,7 +114,9 @@ public class TransactionService {
 
 	@RequestMapping(method = RequestMethod.GET, path = "/outgoing/address/{address}")
 	public ResponseEntity<String> grabOutgoingOnly(@PathVariable String address) {
-
+		totalValue = 0l;
+		totalIncomingValue = 0l;
+		totalOutgoingValue = 0l;
 		net.sf.json.JSONObject json = net.sf.json.JSONObject.fromObject(
 				NetworkUtils.get("http://" + host + ":7890/account/transfers/all?address=" + address).getResponse());
 		net.sf.json.JSONArray jsonArrayMeta = json.getJSONArray("data");
@@ -140,9 +131,6 @@ public class TransactionService {
 		String jsonData = "";
 		while (itr.hasNext()) {
 			JSONObject element = (JSONObject) itr.next();
-
-			// lastHash =
-			// element.getJSONObject("meta").getJSONObject("hash").getString("data");
 			jsonData += parse(address, element.toString(), 2);
 		}
 		jsonData += recurse(address, lastHash, 2);
@@ -168,9 +156,6 @@ public class TransactionService {
 		}
 		while (itr.hasNext()) {
 			JSONObject element = (JSONObject) itr.next();
-
-			// lastHashr =
-			// element.getJSONObject("meta").getJSONObject("hash").getString("data");
 			str += parse(address, element.toString(), type);
 		}
 		if (!lastHashr.equals("")) {
@@ -197,9 +182,9 @@ public class TransactionService {
 			JSONObject otherTrans = transaction.getJSONObject("otherTrans");
 			String recipient = otherTrans.getString("recipient");
 			String sender = KeyConvertor.getAddressFromPublicKey(transaction.getString("signer"));
+			Long amount = Amount.fromMicroNem(otherTrans.getLong("amount")).getNumNem();
 			outJSON.put("sender", KeyConvertor.getAddressFromPublicKey(otherTrans.getString("signer")));
 			outJSON.put("recipient", recipient);
-
 			outJSON.put("amount", Amount.fromMicroNem(otherTrans.getLong("amount")).getNumNem());
 			outJSON.put("date",
 					dateFormat.format(new Date((otherTrans.getLong("timeStamp") + Constants.NEMSISTIME) * 1000)));
@@ -217,53 +202,62 @@ public class TransactionService {
 				outJSON.put("message", "");
 			}
 			// mosaic
-			// if (otherTrans.containsKey("mosaics")) {
-			// JSONArray outMosaicArray = new JSONArray();
-			// JSONArray mosaics = otherTrans.getJSONArray("mosaics");
-			// for (int i = 0; i < mosaics.size(); i++) {
-			// JSONObject outMosaic = new JSONObject();
-			// JSONObject mosaic = mosaics.getJSONObject(i);
-			// long quantity = mosaic.getLong("quantity");
-			// String namespace =
-			// mosaic.getJSONObject("mosaicId").getString("namespaceId");
-			// String mosaicName =
-			// mosaic.getJSONObject("mosaicId").getString("name");
-			// MosaicId mosaicId = new MosaicId(new NamespaceId(namespace),
-			// mosaicName);
-			// MosaicFeeInformation m =
-			// NISQuery.findMosaicFeeInformationByNIS(mosaicId);
-			// outMosaic.put("name", mosaicId.toString());
-			// outMosaic.put("quantity", quantity / Math.pow(10,
-			// m.getDivisibility()));
-			// outMosaicArray.add(outMosaic);
-			// }
-			// if (outMosaicArray.size() != 0) {
-			// outJSON.put("mosaics", outMosaicArray);
-			// }
-			// }
+			if (otherTrans.containsKey("mosaics")) {
+				JSONArray outMosaicArray = new JSONArray();
+				JSONArray mosaics = otherTrans.getJSONArray("mosaics");
+				for (int i = 0; i < mosaics.size(); i++) {
+					JSONObject outMosaic = new JSONObject();
+					JSONObject mosaic = mosaics.getJSONObject(i);
+					long quantity = mosaic.getLong("quantity");
+					String namespace = mosaic.getJSONObject("mosaicId").getString("namespaceId");
+					String mosaicName = mosaic.getJSONObject("mosaicId").getString("name");
+					MosaicId mosaicId = new MosaicId(new NamespaceId(namespace), mosaicName);
+					MosaicFeeInformation m = NISQuery.findMosaicFeeInformationByNIS(mosaicId);
+					outMosaic.put("name", mosaicId.toString());
+					outMosaic.put("quantity", quantity / Math.pow(10, 6));
+					outMosaicArray.add(outMosaic);
+				}
+				if (outMosaicArray.size() != 0) {
+					outJSON.put("mosaics", outMosaicArray);
+				}
+			}else {
+				outJSON.put("mosaics", "");
+			}
 			outJSON.put("hash", hash);
 			outJSON.put("isMultisig", "1");
 			// JSONArray newj = new JSONArray();
 			// outJSON.toJSONArray(newj);
 
 			if (type == 1) { // incoming only
+				
 				if (address.equals(recipient)) {
+					totalIncomingValue += amount;
 					return outJSON.toString() + ",";
 				} else {
 					return "";
 				}
 			} else if (type == 2) {
+				
 				if (address.equals(sender)) {
+					totalOutgoingValue += amount;
 					return outJSON.toString() + ",";
 				} else {
 					return "";
 				}
 			}
+			if (address.equals(recipient)) {
+				totalIncomingValue += amount;
+			}
+			if (address.equals(sender)) {
+				totalOutgoingValue += amount;
+			}
+			totalValue += amount;
 			return outJSON.toString() + ",";
 
 		} else { // normal transaction
 			String recipient = transaction.getString("recipient");
 			String sender = KeyConvertor.getAddressFromPublicKey(transaction.getString("signer"));
+			Long amount = Amount.fromMicroNem(transaction.getLong("amount")).getNumNem();
 			outJSON.put("sender", KeyConvertor.getAddressFromPublicKey(transaction.getString("signer")));
 			outJSON.put("recipient", recipient);
 			outJSON.put("amount", Amount.fromMicroNem(transaction.getLong("amount")).getNumNem());
@@ -284,46 +278,54 @@ public class TransactionService {
 			}
 
 			// mosaic
-			// if (transaction.containsKey("mosaics")) {
-			// JSONArray outMosaicArray = new JSONArray();
-			// JSONArray mosaics = transaction.getJSONArray("mosaics");
-			// for (int i = 0; i < mosaics.size(); i++) {
-			// JSONObject outMosaic = new JSONObject();
-			// JSONObject mosaic = mosaics.getJSONObject(i);
-			// long quantity = mosaic.getLong("quantity");
-			// String namespace =
-			// mosaic.getJSONObject("mosaicId").getString("namespaceId");
-			// String mosaicName =
-			// mosaic.getJSONObject("mosaicId").getString("name");
-			// MosaicId mosaicId = new MosaicId(new NamespaceId(namespace),
-			// mosaicName);
-			// MosaicFeeInformation m =
-			// NISQuery.findMosaicFeeInformationByNIS(mosaicId);
-			// outMosaic.put("name", mosaicId.toString());
-			// outMosaic.put("quantity", quantity / Math.pow(10,
-			// m.getDivisibility()));
-			// outMosaicArray.add(outMosaic);
-			// }
-			// if (outMosaicArray.size() != 0) {
-			// outJSON.put("mosaics", outMosaicArray);
-			// }
-			// }
+			if (transaction.containsKey("mosaics")) {
+				JSONArray outMosaicArray = new JSONArray();
+				JSONArray mosaics = transaction.getJSONArray("mosaics");
+				for (int i = 0; i < mosaics.size(); i++) {
+					JSONObject outMosaic = new JSONObject();
+					JSONObject mosaic = mosaics.getJSONObject(i);
+					long quantity = mosaic.getLong("quantity");
+					String namespace = mosaic.getJSONObject("mosaicId").getString("namespaceId");
+					String mosaicName = mosaic.getJSONObject("mosaicId").getString("name");
+					MosaicId mosaicId = new MosaicId(new NamespaceId(namespace), mosaicName);
+					MosaicFeeInformation m = NISQuery.findMosaicFeeInformationByNIS(mosaicId);
+					outMosaic.put("name", mosaicId.toString());
+					outMosaic.put("quantity", quantity / Math.pow(10, 6));
+					outMosaicArray.add(outMosaic);
+				}
+				if (outMosaicArray.size() != 0) {
+					outJSON.put("mosaics", outMosaicArray);
+				}
+			}else {
+				outJSON.put("mosaics", "");
+			}
 			outJSON.put("hash", hash);
 			outJSON.put("isMultisig", "0");
 
 			if (type == 1) { // incoming only
+				
 				if (address.equals(recipient)) {
+					totalIncomingValue += amount;
 					return outJSON.toString() + ",";
 				} else {
 					return "";
 				}
 			} else if (type == 2) {
+				
 				if (address.equals(sender)) {
+					totalOutgoingValue += amount;
 					return outJSON.toString() + ",";
 				} else {
 					return "";
 				}
 			}
+			if (address.equals(recipient)) {
+				totalIncomingValue += amount;
+			}
+			if (address.equals(sender)) {
+				totalOutgoingValue += amount;
+			}
+			totalValue += amount;
 			return outJSON.toString() + ",";
 
 		}
